0bc2db6a03fe772b4fdcc32873b64ed88a1a49c8
nataraj <natarajv@kriyatec.com>
2024-10-04 20:24:52 +0530

    full completed the ui in jason

--- src/app/component/dynamic-question/dynamic-question.component.ts
+++ src/app/component/dynamic-question/dynamic-question.component.ts
@@ -12,8 +12,10 @@ import { map } from 'rxjs/operators';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatButtonModule } from '@angular/material/button';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { HelpsService } from '../../services/helps.service';
+import { F } from '@angular/cdk/keycodes';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-dynamic-question',
@@ -30,23 +32,32 @@ import { HelpsService } from '../../services/helps.service';
     MatDatepickerModule,
     MatNativeDateModule,
     MatButtonModule,
-    CommonModule
+    CommonModule,
+    HttpClientModule, 
   ],
+  
 
   templateUrl: './dynamic-question.component.html',
   styleUrls: ['./dynamic-question.component.css']
 })
 export class DynamicQuestionComponent implements OnInit {
 
-  constructor(public formBuilder: FormBuilder, private http: HttpClient, private helperServices: HelpsService) { }
+  constructor(
+    public formBuilder: FormBuilder, 
+    private http: HttpClient, 
+    private helperServices: HelpsService,
+    private dataService: DataService
+  ) 
+    { }
 
+  apiUrl = 'http://localhost:3000/ael/add';
   currentQuestionIndex = 0;
   @Input() item: any = '';
   options!: FormGroup;
   questions: any = [];
   profileSummary: { [key: string]: any } = {};
   selectedRaces: string[] = [];
-
+  profiledetails: any = []
 
 
   ngOnChanges(changes: SimpleChanges): void {
@@ -66,8 +77,14 @@ export class DynamicQuestionComponent implements OnInit {
         this.currentQuestionIndex = 0
         this.options = new FormGroup({})
         this.questions = response;
+        this.profiledetails = []
         this.questions.forEach((fieldarr: any) => {
           fieldarr.fields.forEach((field: any) => {
+            let value: any = {}
+            value['key'] = field.name
+            value['label'] = field.label
+            value['value'] = ""
+            this.profiledetails.push(value)
             if (field.type === 'checkbox') {
               const formArray = this.formBuilder.array(field.options.map(() => new FormControl(false)));
               this.options.addControl(field.name, formArray);
@@ -94,23 +111,67 @@ export class DynamicQuestionComponent implements OnInit {
     this.options = this.formBuilder.group({
     });
     this.loadInit("personal")
-
   }
 
   configloaded = true
+
   get isFormValid(): boolean {
-    return true;
-    // return this.options.valid; 
+    // return true;
+    return this.options.valid;
 
   }
+
+
 
   addvalue(name: any, value: any) {
     this.options.get(name)?.setValue(value)
   }
 
-  nextQuestion(): void {
+  saveFormData() {
+    console.log("profileSummary" ,this.profileSummary);
+    console.log("profiledetails" ,this.profiledetails);
+    console.log("this.options.value" ,this.options.value);
+    
+    this.http.post(this.apiUrl, this.options.value).subscribe((res: any) => {
+      console.log(res)
+    });
+  }
 
-    // this.profileSummary = this.options.value;
+  nextQuestion(): void {
+    this.saveFormData()
+    
+    const currentQuestion = this.questions[this.currentQuestionIndex];
+
+    const requiredFields = currentQuestion.fields.filter((field: any) => field.required);
+
+    const isCurrentQuestionValid = requiredFields.every((field: any) => {
+      return this.options.get(field.name)?.valid;
+    });
+    if (isCurrentQuestionValid) {
+
+      this.currentQuestionIndex++;
+      this.helperServices.setSidenavBehaviour(this.questions[this.currentQuestionIndex])
+      let prevIndex = Number(this.currentQuestionIndex);
+      --prevIndex
+      this.questions[prevIndex].fields.forEach((element: any) => {
+        let currentSummaryvalue = this.profiledetails.find((res: any) => res.key === element.name);
+
+        if (currentSummaryvalue) {
+          // Update the value if the element is found in profiledetails
+          currentSummaryvalue['value'] = this.options.get(element.name)?.value || '';
+
+          console.log("Question Name:", element.name);
+          console.log("Question Details:", currentSummaryvalue);
+        } else {
+          // If no match is found, log it or handle it accordingly
+          console.warn(`No matching entry found for ${element.name} in profiledetails`);
+        }
+      });
+      console.log("Summary Details ", this.profiledetails);
+
+    } else {
+
+    }
 
     const optionsArray: any = [];
     this.questions.forEach((questionData: any) => {
@@ -122,26 +183,12 @@ export class DynamicQuestionComponent implements OnInit {
             value: this.options.get(field.name)?.value || 'Not provided2'
           };
         }
-
         return null;
       }).filter((field: any) => field !== null);
 
       optionsArray.push(...fieldsArray);
     });
-
-
     this.profileSummary = optionsArray;
-    // console.warn(optionsArray)
-
-    if (this.isFormValid) {
-
-      this.currentQuestionIndex++;
-      this.helperServices.setSidenavBehaviour(this.questions[this.currentQuestionIndex])
-
-
-    } else {
-      this.options.markAllAsTouched();
-    }
   }
 
   prevQuestion(): void {
@@ -151,6 +198,16 @@ export class DynamicQuestionComponent implements OnInit {
 
     }
   }
+
+  isCurrentQuestionValid(): boolean {
+    const currentQuestion = this.questions[this.currentQuestionIndex];
+    const requiredFields = currentQuestion.fields.filter((field: any) => field.required);
+
+    return requiredFields.every((field: any) => {
+      return this.options.get(field.name)?.valid;
+    });
+  }
+
 
   formatValue(key: string, value: any = "notgiven"): string {
     if (value == "notgiven") {
@@ -184,8 +241,7 @@ export class DynamicQuestionComponent implements OnInit {
   }
 
   isLastQuestion(): boolean {
-    return this.currentQuestionIndex === this.questions.length - 1 ;
-}
-
+    return this.currentQuestionIndex === this.questions.length - 1;
+  }
 
 }
